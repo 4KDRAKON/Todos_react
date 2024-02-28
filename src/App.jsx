b3c61b4a8b071fded930f524e3f7f4877da99d94
4kdrakon <Uzbcordinalochilov@gmail.com>
2024-02-28 17:03:28 +0500

    Todos

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => toggleTodo(index)}>Toggle</button>
+            
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
+
---
+++ src/components/nima/salom.jsx
@@ -0,0 +1,15 @@
+// function nima(){
+//     return(
+
+       
+//         <>
+        
+//         <form className="salom">
+//             <input className="nimadir" type="text" />
+//             <button className="btn">qo'shish</button>
+//         </form>
+//         </>
+//     )
+// }
+
+// export default nima;
